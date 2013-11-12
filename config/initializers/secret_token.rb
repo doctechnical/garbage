88d9ad37d9be9e966fb3ad13b196a6420a0738e4
Monte Ferguson <fnord@hellokitty.com>
2013-11-12 14:20:20 -0500

    It has to start somewhere

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garbage::Application.config.secret_token = 'b8a03bc80c4a2d75c8f56e9956260c78f3b68d8120611840b2a257baaa571df84748935efbbc42f22596ef473d3c1a86630887bef62b84dee7710c742b1a34aa'
